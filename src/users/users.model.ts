8ee65e791c8d389bb6b7df6d2e9ba15c85c431aa
Pavel Krikunov <fusrodah.df@mail.ru>
2023-05-28 15:51:28 +0300

    create table for user

---
+++ src/users/users.model.ts
@@ -0,0 +1,14 @@
+import {Column, Model, Table} from "sequelize-typescript";
+
+@Table
+export class Users extends Model {
+    @Column
+    username: string;
+
+    @Column
+    password: string;
+
+    @Column
+    email: string;
+
+}
