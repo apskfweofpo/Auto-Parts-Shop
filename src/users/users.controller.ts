e3f699c6959620aace62ee136d8f9da9be73087e
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-07 02:05:06 +0300

    add post request for create user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Header, HttpCode, HttpStatus, Post} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {HTTP_CODE_METADATA} from "@nestjs/common/constants";
+import {constants} from "http2";
+import {CreateUserDto} from "./dto/createUserDto";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {
+    }
+
+    @Post('/signup')
+    @HttpCode(HttpStatus.CREATED)
+    @Header('Content-type', 'application/json')
+    createUser(@Body() createUserDto: CreateUserDto) {
+        return this.userService.create(createUserDto)
+    }
+}
