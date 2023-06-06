279742b3d2ab663d84417e140ea0355f8bb430de
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-07 02:04:36 +0300

    add AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import {SequelizeModule} from "@nestjs/sequelize";
 import {ConfigModule} from "@nestjs/config";
 import {SequelizeConfigService} from "./config/sequelizeConfig.service";
 import {databaseConfig} from "./config/configuration";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import {databaseConfig} from "./config/configuration";
       ConfigModule.forRoot({
         load: [databaseConfig]
       }),
-      UsersModule],
+      UsersModule,
+      AuthModule],
 })
 export class AppModule {}
