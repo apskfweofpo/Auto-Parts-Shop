63df6add605121eb4fd2ed1921d859191dd330af
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-07 23:48:24 +0300

    create class for sessions

---
+++ src/auth/session.serializer.ts
@@ -0,0 +1,13 @@
+import {Injectable} from "@nestjs/common";
+import {PassportSerializer} from "@nestjs/passport";
+
+@Injectable()
+export class SessionSerializer extends PassportSerializer {
+    deserializeUser(payload: any, done: Function): any {
+        done(null, payload)
+    }
+
+    serializeUser(user: any, done: Function): any {
+        done(null, user)
+    }
+}
