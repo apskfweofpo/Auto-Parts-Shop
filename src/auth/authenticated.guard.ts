1597e69fa4050e2cafffb53ea0950a9b6c18a06d
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-08 13:18:27 +0300

    add async for func canActivate

--- src/auth/authenticated.guard.ts
+++ src/auth/authenticated.guard.ts
@@ -1,11 +1,10 @@
-import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthenticatedGuard implements CanActivate{
-    canActivate(context: ExecutionContext) {
+export class AuthenticatedGuard implements CanActivate {
+    async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest();
 
         return request.isAuthenticated();
     }
-
 }
