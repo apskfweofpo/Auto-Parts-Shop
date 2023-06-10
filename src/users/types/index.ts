dc3fb8d67181fc2f018399652516cd7adec8706e
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-11 01:08:40 +0300

    add class for LogoutUserResponse, LoginCheckResponse, SignupResponse

--- src/users/types/index.ts
+++ src/users/types/index.ts
@@ -29,4 +29,38 @@ export class LoginUserResponse {
 }
 
 
+export class LogoutUserResponse {
+    @ApiProperty({example: 'session has ended'})
+    msg: string;
+}
 
+export class LoginCheckResponse {
+    @ApiProperty({example: '1'})
+    userId: string;
+
+    @ApiProperty({example: 'Ivan'})
+    username: string;
+
+    @ApiProperty({example: 'Ivan@gmail.ru'})
+    email: string;
+}
+
+export class SignupResponse {
+    @ApiProperty({example: '3'})
+    id: string;
+
+    @ApiProperty({example: 'Ivan'})
+    username: string;
+
+    @ApiProperty({example: 'Ivan@gmail.ru'})
+    email: string;
+
+    @ApiProperty({example: '$2b$04$wNtlqNWz5vj3qb0hcfVWS.ySV5di0cNfQ/naU9wzFFHtPlCcVXHvi'})
+    password: string;
+
+    @ApiProperty({example: '2023-06-10T22:03:41.919Z'})
+    updatedAt: string;
+
+    @ApiProperty({example: '2023-06-10T22:03:41.919Z'})
+    createdAt: string;
+}
