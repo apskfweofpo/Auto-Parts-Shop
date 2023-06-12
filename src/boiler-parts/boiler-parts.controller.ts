d672da4e2b98ea9cdf11ac5b7cba38bffcf922e5
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-12 15:01:33 +0300

    add types for response

--- src/boiler-parts/boiler-parts.controller.ts
+++ src/boiler-parts/boiler-parts.controller.ts
@@ -1,50 +1,70 @@
-import {Body, Controller, Get, Param, Post, Query, UseGuards} from '@nestjs/common';
-import {InjectModel} from "@nestjs/sequelize";
-import {BoilerPartsService} from "./boiler-parts.service";
-import {AuthenticatedGuard} from "../auth/authenticated.guard";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { BoilerPartsService } from './boiler-parts.service';
+import { AuthenticatedGuard } from '../auth/authenticated.guard';
+import {ApiBody, ApiOkResponse} from "@nestjs/swagger";
+import {
+  FindOneResponse,
+  GetBestsellersResponse, GetByNameRequest,
+  GetByNameResponse,
+  GetNewResponse,
+  PaginateAndFilterResponse, SearchRequest, SearchResponse
+} from "./types";
 
 @Controller('boiler-parts')
 export class BoilerPartsController {
+  constructor(private readonly boilerPartsService: BoilerPartsService) {}
 
-    constructor(
-        private readonly boilerPartsService: BoilerPartsService
-    ) {
-    }
+  @ApiOkResponse({type: PaginateAndFilterResponse})
+  @UseGuards(AuthenticatedGuard)
+  @Get()
+  paginateAndFilter(@Query() query) {
+    return this.boilerPartsService.paginateAndFilter(query);
+  }
 
-    @UseGuards(AuthenticatedGuard)
-    @Get()
-    paginateAndFilter(@Query() query) {
-        return this.boilerPartsService.paginateAndFilter(query)
-    }
+  @ApiOkResponse({type: FindOneResponse})
+  @UseGuards(AuthenticatedGuard)
+  @Get('find/:id')
+  getOne(@Param('id') id: string) {
+    return this.boilerPartsService.findOne(id);
+  }
 
-    @UseGuards(AuthenticatedGuard)
-    @Get('find/:id')
-    getOne(@Param('id') id: string) {
-        return this.boilerPartsService.findOne(id);
-    }
+  @ApiOkResponse({type: GetBestsellersResponse})
+  @UseGuards(AuthenticatedGuard)
+  @Get('bestsellers')
+  getBestseller() {
+    return this.boilerPartsService.bestsellers();
+  }
 
-    @UseGuards(AuthenticatedGuard)
-    @Get('bestsellers')
-    getBestseller() {
-        return this.boilerPartsService.bestsellers();
-    }
+  @ApiOkResponse({type: GetNewResponse})
+  @UseGuards(AuthenticatedGuard)
+  @Get('new')
+  getNew() {
+    return this.boilerPartsService.new();
+  }
+
+  @ApiOkResponse({type: SearchResponse})
+  @ApiBody({type: SearchRequest})
+  @UseGuards(AuthenticatedGuard)
+  @Post('search')
+  search(@Body() { search }: { search: string }) {
+    return this.boilerPartsService.searchByString(search);
+  }
 
 
-    @UseGuards(AuthenticatedGuard)
-    @Get('new')
-    getNew() {
-        return this.boilerPartsService.new();
-    }
 
-    @UseGuards(AuthenticatedGuard)
-    @Post('search')
-    search(@Body() {search}: { search: string }) {
-        return this.boilerPartsService.searchByString(search);
-    }
-
-    @UseGuards(AuthenticatedGuard)
-    @Post('name')
-    searchByName(@Body() {name}: { name: string }) {
-        return this.boilerPartsService.findOneByName(name);
-    }
+  @ApiOkResponse({type: GetByNameResponse})
+  @ApiBody({type: GetByNameRequest})
+  @UseGuards(AuthenticatedGuard)
+  @Post('name')
+  searchByName(@Body() { name }: { name: string }) {
+    return this.boilerPartsService.findOneByName(name);
+  }
 }
