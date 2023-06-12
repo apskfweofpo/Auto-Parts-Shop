73c8afa1577bf5ed6e19726256b84c05de1da718
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-12 17:55:47 +0300

    add find all and add and update count

--- src/shopping-cart/shopping-cart.service.ts
+++ src/shopping-cart/shopping-cart.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {ShoppingCart} from "./shopping-cart.model";
+import {UsersService} from "../users/users.service";
+import {AddToCartDto} from "./dto/add-to-cart.dto";
+import {BoilerPartsService} from "../boiler-parts/boiler-parts.service";
+import {InjectModel} from "@nestjs/sequelize";
 
 @Injectable()
-export class ShoppingCartService {}
+export class ShoppingCartService {
+    constructor(
+        @InjectModel(ShoppingCart)
+        private readonly shoppingCart: typeof ShoppingCart,
+        private readonly usersService: UsersService,
+        private readonly boilerParts: BoilerPartsService
+    ) {
+    }
+
+    async findAll(userId: number | string): Promise<ShoppingCart[]> {
+        return this.shoppingCart.findAll({
+            where: {id: userId}
+        })
+    }
+
+    async add(addToCartDto: AddToCartDto) {
+        const cart = new ShoppingCart();
+        const user = await this.usersService.findOne({
+            where: {username: addToCartDto.username},
+        });
+        const part = await this.boilerParts.findOne(addToCartDto.partId)
+
+        cart.user_id = user.id;
+        cart.part_id = part.id;
+        cart.boiler_manufacturer = part.boiler_manufacturer;
+        cart.parts_manufacturer = part.parts_manufacturer;
+        cart.price = part.price;
+        cart.in_stock = part.in_stock;
+        cart.image = JSON.stringify(part.images)[0];
+        cart.name = part.name;
+        cart.total_price = part.price;
+
+        return cart.save();
+    }
+
+    async updateCount(count: number, partId: number): Promise<{ count: number }> {
+        await this.shoppingCart.update({count}, {where: {partId}})
+
+        const part = await this.shoppingCart.findOne({where: {partId}});
+        return {count: part.count}
+    }
+}
