5afcee13a7e38e94db0d628e6d67ddc94319fbf4
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-12 17:54:47 +0300

    add dto for add to cart

---
+++ src/shopping-cart/dto/add-to-cart.dto.ts
@@ -0,0 +1,17 @@
+import {ApiProperty} from "@nestjs/swagger";
+import {IsNotEmpty, IsOptional} from "class-validator";
+
+export class AddToCartDto {
+    @ApiProperty({example: 'Ivan'})
+    @IsNotEmpty()
+    readonly username: string;
+
+    @ApiProperty({example: 1})
+    @IsOptional()
+    readonly userId?: number;
+
+    @ApiProperty({example: 1})
+    @IsNotEmpty()
+    readonly partId: string
+
+}
