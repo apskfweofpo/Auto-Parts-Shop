a5537a5245ec6f9c8fb1fac5669ff0a379594db0
Pavel Krikunov <fusrodah.df@mail.ru>
2023-06-14 23:50:20 +0300

    add tests for login and login-check

---
+++ test/controllers/auth.controller.spec.ts
@@ -0,0 +1,93 @@
+import {INestApplication} from "@nestjs/common";
+import {Test, TestingModule} from "@nestjs/testing";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeConfigService} from "../../src/config/sequelizeConfig.service";
+import {databaseConfig} from "../../src/config/configuration";
+import {UsersModule} from "../../src/users/users.module";
+import {User} from "../../src/users/users.model";
+import * as bcrypt from "bcrypt";
+import * as request from "supertest";
+import * as session from "express-session";
+import * as passport from "passport";
+import {AuthModule} from "src/auth/auth.module";
+
+const mockedUser = {
+    username: 'Jhon',
+    email: "jhon@gmail.com",
+    password: 'jhon123'
+}
+
+describe('Auth controller', () => {
+    let app: INestApplication;
+
+    beforeEach(async () => {
+        const testModule: TestingModule = await Test.createTestingModule({
+            imports: [SequelizeModule.forRootAsync({
+                imports: [ConfigModule],
+                useClass: SequelizeConfigService
+            }),
+                ConfigModule.forRoot({
+                    load: [databaseConfig]
+                }),
+                AuthModule
+            ]
+        }).compile();
+
+        app = testModule.createNestApplication();
+
+        app.use(
+            session({
+                secret: 'keyword',
+                resave: false,
+                saveUninitialized: false,
+            }),
+        );
+        app.use(passport.initialize());
+        app.use(passport.session());
+
+        await app.init();
+    })
+    beforeEach(async () => {
+        const user = new User();
+
+        const hashedPassword = await bcrypt.hash(mockedUser.password, 3);
+
+        user.username = mockedUser.username;
+        user.email = mockedUser.email;
+        user.password = hashedPassword;
+
+        return user.save();
+    })
+
+    afterEach(async () => {
+        await User.destroy({where: {username: mockedUser.username}})
+    })
+
+    it('Should login user', async () => {
+
+        const response = await request(app.getHttpServer())
+            .post('/users/login')
+            .send({username: mockedUser.username, password: mockedUser.password});
+
+        expect(response.body.user.username).toBe(mockedUser.username);
+        expect(response.body.user.email).toBe(mockedUser.email);
+        expect(response.body.msg).toBe("Logged in");
+
+    })
+
+
+    it('Should login-check user', async () => {
+
+        const login = await request(app.getHttpServer())
+            .post('/users/login')
+            .send({username: mockedUser.username, password: mockedUser.password});
+
+        const loginCheck = await request(app.getHttpServer())
+            .get('/users/login-check')
+            .set('Cookie', login.headers['set-cookie']);
+
+        expect(loginCheck.body.username).toBe(mockedUser.username);
+        expect(loginCheck.body.email).toBe(mockedUser.email);
+    })
+})
